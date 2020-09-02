974be3305f8bee39acb38e49d19fab038ca17257
Dito Abdusalam Rafi <44570047+dabdusalam@users.noreply.github.com>
2020-09-02 08:32:42 +0700

    Update tugas7.js

--- Tugas-7/tugas7.js
+++ Tugas-7/tugas7.js
@@ -1,6 +1,6 @@
 //SOAL1
 
-/**/
+/*
 class Animal {
 	constructor(nama) {
 		this.hewaname = nama;
@@ -51,4 +51,49 @@ console.log(sungokong.yell()) // "Auooo"
  
 var kodok = new Frog("buduk")
 console.log(kodok.jump()) // "hop hop" 
+
+*/
+
+//SOAL2
+class Clock{
+	constructor({template}){
+		this.template = template;
+		this.timer = null;
+
+ 	 var timer;
+	}
+
+  render() {
+    var date = new Date();
+
+    var hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    var mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    var secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    var output = template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  this.stop = function() {
+    clearInterval(timer);
+  };
+
+  this.start = function() {
+    render();
+    timer = setInterval(render, 1000);
+  };
+
+}
+
+var clock = new Clock({template: 'h:m:s'});
+clock.start(); 
 
