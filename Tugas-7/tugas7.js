5713b2504d23ebcb2cd11195e3e93cecc25a64ba
Dito Abdusalam Rafi <44570047+dabdusalam@users.noreply.github.com>
2020-09-02 07:58:49 +0700

    Add files via upload

---
+++ Tugas-7/tugas7.js
@@ -0,0 +1,40 @@
+//SOAL1
+
+/**/
+class Animal {
+	constructor(nama) {
+		this.hewaname = nama;
+		this.kaki = 4;
+		this.cold = "false";		
+	}
+	get name(){
+		return this.hewaname;
+	}
+	get legs(){
+		//return this.hewaname.length;
+		return this.kaki;
+	}
+	get cold_blooded(){		
+		return this.cold;
+	}
+    // Code class di sini
+}
+class Ape extends Animal{
+
+}
+class 
+
+ //RELASE0
+var sheep = new Animal("shaun");
+ 
+console.log(sheep.name) // "shaun"
+console.log(sheep.legs) // 4
+console.log(sheep.cold_blooded) // false
+
+//RELASE0
+var sungokong = new Ape("kera sakti")
+console.log(sungokong.yell()) // "Auooo"
+ 
+var kodok = new Frog("buduk")
+console.log(kodok.jump()) // "hop hop" 
+
